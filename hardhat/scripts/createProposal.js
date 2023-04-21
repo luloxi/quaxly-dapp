7ea9888632eb03c84ac2978dfec809658fca1671
Lulox <ligma@duck.com>
2023-04-21 12:35:48 -0300

    First working createProposal script!

--- hardhat/scripts/createProposal.js
+++ hardhat/scripts/createProposal.js
@@ -1,6 +1,13 @@
+const { deployments, ethers } = require("hardhat")
+const {
+  developmentChains,
+  DAO_MODERATORS: { NEW_MODERATORS, SET_NEW_MODERATOR_FN },
+} = require("../helper-hardhat-config")
+
+const proposalDescription = "Example proposal description"
+
 const createProposal = async (GovernorContract, GovernanceToken, DAOModerators, moderatorIndex) => {
   const calldata = getCalldata(DAOModerators, moderatorIndex)
-  // eslint-disable-next-line no-undef
   const [{ address: owner }] = await ethers.getSigners()
   await GovernanceToken.delegate(owner)
 
@@ -22,15 +29,47 @@ const createProposal = async (GovernorContract, GovernanceToken, DAOModerators, 
   }
 }
 
-const writeProposal = async function () {
+const getCalldata = (DAOModerators, moderatorIndex) => {
+  const { NAME, EMAIL, MODERATOR_ADDRESS } = NEW_MODERATORS[moderatorIndex]
+  return DAOModerators.interface.encodeFunctionData(SET_NEW_MODERATOR_FN, [
+    NAME,
+    EMAIL,
+    MODERATOR_ADDRESS,
+  ])
+}
+
+async function main() {
+  /* Get signer and contract */
+
+  const accounts = await ethers.getSigners()
+  const deployer = accounts[0]
+
+  const daoModerators = await ethers.getContract("DAOModerators", deployer)
+  const governanceToken = await ethers.getContract("GovernanceToken", deployer)
+  const governorContract = await ethers.getContract("GovernorContract", deployer)
+
+  // /* Creating a new pproposal */
+
   const { proposalId } = await createProposal(governorContract, governanceToken, daoModerators, 0)
 
-  let _proposalState = await governorContract.state(proposalId)
-  expect(_proposalState).to.equal(proposalState.pending)
+  let proposalState = await governorContract.state(proposalId)
+  console.log(`Current proposal state of proposal # ${proposalId} is: ${proposalState}`)
 
   // If development chains blabla
-  await moveBlocks(INITIAL_VOTING_DELAY + 1)
+  // await moveBlocks(INITIAL_VOTING_DELAY + 1)
+  // let newProposalState = await governorContract.state(proposalId)
+  // console.log("Now proposal state is:", newProposalState)
 
-  _proposalState = await governorContract.state(proposalId)
-  expect(_proposalState).to.equal(proposalState.active)
+  console.log("Done!")
 }
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error)
+    process.exit(1)
+  })
+
+// const writeProposal = async function () {
+
+// }
