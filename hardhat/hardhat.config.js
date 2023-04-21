d82e029cacdbe9bf88788c3d7daaa2b9f5680d00
Lulox <ligma@duck.com>
2023-04-21 00:55:52 -0300

    Added fix for using the repo without private key

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -3,7 +3,10 @@ require("@nomiclabs/hardhat-etherscan")
 require("dotenv").config()
 require("hardhat-deploy")
 
-const PRIVATE_KEY = process.env.PRIVATE_KEY || ""
+const PRIVATE_KEY =
+  process.env.PRIVATE_KEY || "0xdbda1821b80551c9d65939329250298aa3472ba22feea921c0cf5d620ea67b97"
+// Placeholder to enable hardhat tests without filling out the PRIVATE_KEY variable in the .env file
+// DO NOT USE THIS PRIVATE KEY FOR INTERACTING WITH REAL FUNDS
 
 const MAINNET_RPC_URL = process.env.MAINNET_RPC_URL || ""
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ""
